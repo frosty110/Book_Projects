29b17c144e82bf6858006cca3ae6adbdb5b40276
Blaise Albuquerque <frost01@gmail.com>
2018-02-14 02:19:23 -0800

    Updated git ignore. Updated to chapter 4.

--- Data Science/Python Data Science Essentials/ch_03_Line_chat_trends.py
+++ Data Science/Python Data Science Essentials/ch_03_Line_chat_trends.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 
-variance     = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
 total_error  = [x + y for x, y in zip(variance, bias_squared)]
 xs = [i for i, _ in enumerate(variance)]
